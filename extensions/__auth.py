b440b40503488b9a83d03cbe543983797b7b3453
Yiays <yesiateyoursheep@gmail.com>
2022-03-04 23:32:28 +0000

    Upstream improvements from merelybot

--- extensions/__auth.py
+++ extensions/__auth.py
@@ -13,19 +13,16 @@ class Auth(commands.Cog):
     if 'authusers' not in bot.config['auth']:
       bot.config['auth']['authusers'] = ''
   
-  #TODO: figure out why this isn't working
-  @commands.Cog.listener("on_command_error")
-  async def check_autherror(self, ctx:commands.Context, error):
-    if isinstance(error, commands.errors.CommandInvokeError):
-      if error.original is AuthError:
-        await ctx.reply(str(error.original))
+  class AuthError(Exception):
+    """Errors to be sent to a user that failed an auth test"""
+    pass
 
   def owners(self, ctx:commands.Context):
       if ctx.message.author == ctx.message.guild.owner or\
          str(str(ctx.message.author.id)) in self.bot.config['auth']['superusers']:
         return True
       else:
-        raise AuthError(self.bot.babel(ctx, 'auth', 'unauthorized'))
+        raise self.AuthError(self.bot.babel(ctx, 'auth', 'unauthorized'))
 
   def admins(self, ctx:commands.Context):
       if ctx.message.author == ctx.message.guild.owner or\
@@ -33,7 +30,7 @@ class Auth(commands.Cog):
          str(ctx.message.author.id) in self.bot.config['auth']['superusers']:
         return True
       else:
-        raise AuthError(self.bot.babel(ctx, 'auth', 'not_admin'))
+        raise self.AuthError(self.bot.babel(ctx, 'auth', 'not_admin'))
 
   def mods(self, ctx:commands.Context):
       if ctx.message.author == ctx.message.guild.owner or\
@@ -43,24 +40,20 @@ class Auth(commands.Cog):
          str(ctx.message.author.id) in self.bot.config['auth']['authusers']:
         return True
       else:
-        raise AuthError(self.bot.babel(ctx, 'auth', 'not_mod'))
+        raise self.AuthError(self.bot.babel(ctx, 'auth', 'not_mod'))
 
   def superusers(self, ctx:commands.Context):
       if str(ctx.message.author.id) in self.bot.config['auth']['superusers']:
         return True
       else:
-        raise AuthError(self.bot.babel(ctx, 'auth', 'not_superuser'))
+        raise self.AuthError(self.bot.babel(ctx, 'auth', 'not_superuser'))
 
   def authusers(self, ctx:commands.Context):
       if str(ctx.message.author.id) in self.bot.config['auth']['superusers'] or\
          str(ctx.message.author.id) in self.bot.config['auth']['authusers']:
         return True
       else:
-        raise AuthError(self.bot.babel(ctx, 'auth', 'not_authuser'))
-
-class AuthError(Exception):
-  """Errors to be sent to a user that failed an auth test"""
-  pass
+        raise self.AuthError(self.bot.babel(ctx, 'auth', 'not_authuser'))
 
 def setup(bot):
   bot.add_cog(Auth(bot))
