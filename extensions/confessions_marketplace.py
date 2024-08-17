7e3ed4cdffa44274521cd8620aa769afa7d39e8d
Yiays <yesiateyoursheep@gmail.com>
2024-08-17 21:41:25 +1200

    Complete embed generation for /sell

--- extensions/confessions_marketplace.py
+++ extensions/confessions_marketplace.py
@@ -42,31 +42,39 @@ class ConfessionsMarketplace(commands.Cog):
     self,
     inter: disnake.GuildCommandInteraction,
     title: str = commands.Param(max_length=80),
+    starting_price: str = commands.Param(max_length=10),
+    payment_methods: str = commands.Param(min_length=3, max_length=60),
     description: Optional[str] = commands.Param(default=None, max_length=1000),
     image: Optional[disnake.Attachment] = None
   ):
     """
-    Start an anonymous listing
+      Start an anonymous listing
 
-    Parameters
-    ----------
-    title: A short summary of the item you are selling
-    description: Further details about the item you are selling
-    image: A picture of the item you are selling
+      Parameters
+      ----------
+      title: A short summary of the item you are selling
+      starting_price: The price you would like to start bidding at, in whatever currency you accept
+      payment_methods: Payment methods you will accept, PayPal, Venmo, Crypto, etc.
+      description: Further details about the item you are selling
+      image: A picture of the item you are selling
     """
     guildchannels = get_guildchannels(self.config, inter.guild_id)
     if inter.channel_id not in guildchannels:
       await inter.send(self.babel(inter, 'nosendchannel'), ephemeral=True)
       return
-    if guildchannels[inter.channel_id] != ChannelType.marketplace:
+    if guildchannels[inter.channel_id] != ChannelType.marketplace():
       await inter.send(self.babel(inter, 'wrongcommand', cmd='confess'), ephemeral=True)
       return
 
     clean_desc = description.replace('# ', '') if description else '' # TODO: do this with regex
     embed = disnake.Embed(title=title, description=clean_desc)
+    embed.add_field('Starting price:', starting_price, inline=True)
+    embed.add_field('Accepted payment methods:', payment_methods, inline=True)
+    embed.set_footer(text=self.babel(inter, 'shop_disclaimer'))
 
+    # We provide description as content for the spam checker, otherwise this is unused
     await self.bot.cogs['Confessions'].confess(
-      inter, embed=embed
+      inter, description if description else title, image=image, embed=embed
     )
 
 
