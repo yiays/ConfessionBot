96d4e285c24c64a833466b8d79018a2f86f9d72a
Yiays <yesiateyoursheep@gmail.com>
2024-09-01 17:57:51 +1200

    (Untested) Offer withdrawl, offer accepting, listing withdrawl implemented

--- extensions/confessions_marketplace.py
+++ extensions/confessions_marketplace.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
   from babel import Resolvable
   from configparser import SectionProxy
 
-from overlay.extensions.confessions_common import ChannelType, get_guildchannels
+from overlay.extensions.confessions_common import ChannelType, get_guildchannels, ConfessionData
 
 
 class ConfessionsMarketplace(commands.Cog):
@@ -82,22 +82,87 @@ class ConfessionsMarketplace(commands.Cog):
       embed.add_field('Offer payment method:', inter.text_values['offer_method'], inline=True)
       embed.set_footer(text=self.parent.babel(inter, 'shop_disclaimer'))
 
-      # We provide description as content for the spam checker, otherwise this is unused
-      await self.parent.bot.cogs['Confessions'].confess(
-        inter, embed.title, embed=embed, reference=self.origin.message
-      )
-      # TODO: Disallow making offers to yourself, or repeat offers?
+      pendingconfession = ConfessionData(self.bot.cogs['Confessions'])
+      pendingconfession.create(inter.author, inter.channel, reference=self.origin.message)
+      pendingconfession.set_content(embed=embed)
+
+      if vetting := await pendingconfession.check_vetting(inter):
+        await self.parent.bot.cogs['ConfessionsModeration'].send_vetting(
+          inter, pendingconfession, vetting
+        )
+        return
+      if vetting is False:
+        return
+      await pendingconfession.send_confession(inter, True, webhook_override=False)
 
   # Events
 
   @commands.Cog.listener('on_button_click')
+  async def check_button_click(self, inter:disnake.MessageInteraction):
+    """ Check the button press events and handle relevant ones """
+    if inter.data.custom_id.startswith('confessionmarketplace_offer'):
+      return await self.on_make_offer(inter)
+    if inter.data.custom_id.startswith('confessionmarkeplace_accept'):
+      return await self.on_accept_offer(inter)
+    if inter.data.custom_id.startswith('confessionmarketplace_withdraw'):
+      return await self.on_withdraw(inter)
+
   async def on_make_offer(self, inter:disnake.MessageInteraction):
     """ Open the offer form when a user wants to make an offer on a listing """
-    if inter.data.custom_id.startswith('confessionmarketplace_offer'):
-      if len(inter.message.embeds) == 0:
-        await inter.send(self.babel(inter, 'error_embed_deleted'), ephemeral=True)
-        return
-      await inter.response.send_modal(self.OfferModal(self, inter))
+    if len(inter.message.embeds) == 0:
+      await inter.send(self.babel(inter, 'error_embed_deleted'), ephemeral=True)
+      return
+    if len(inter.data.custom_id) < 30:
+      await inter.send(self.babel(inter, 'error_old_offer'), ephemeral=True)
+    id_seller = inter.data.custom_id[28:]
+    id_buyer = (
+      self.bot.cogs['Confessions'].crypto.encrypt(inter.author.id.to_bytes(8, 'big')).decode('ascii')
+    )
+    if id_seller == id_buyer:
+      await inter.send(self.babel(inter, 'error_self_offer'), ephemeral=True)
+    await inter.response.send_modal(self.OfferModal(self, inter))
+
+  async def on_accept_offer(self, inter:disnake.MessageInteraction):
+    listing = await inter.channel.fetch_message(inter.message.reference.message_id)
+    if len(listing.embeds) == 0 or len(inter.message.embeds) == 0:
+      await inter.send(self.babel(inter, 'error_embed_deleted'), ephemeral=True)
+      return
+    encrypted_data = listing.components[0].children[0].custom_id[29:].split('_')
+    seller_id = int.from_bytes(self.bot.cogs['Confessions'].crypto.decrypt(encrypted_data[0]), 'big')
+    buyer_id = int.from_bytes(self.bot.cogs['Confessions'].crypto.decrypt(encrypted_data[1]), 'big')
+    if seller_id == inter.author.id:
+      seller = inter.author
+      buyer = await inter.guild.getch_member(buyer_id)
+    else:
+      await inter.send(self.babel(inter, 'error_wrong_person'), ephemeral=True)
+      return
+    receipts = [listing.embeds[0], inter.message.embeds[0]]
+    await seller.send(
+      self.babel(inter, 'sale_complete', sell=True, other=buyer.mention), embeds=receipts
+    )
+    await buyer.send(
+      self.babel(inter, 'sale_complete', sell=False, other=seller.mention), embeds=receipts
+    )
+    await inter.edit_original_message(content=self.babel(inter, 'offer_accepted'), components=None)
+
+  async def on_withdraw(self, inter:disnake.MessageInteraction):
+    encrypted_data = inter.data.custom_id[29:].split('_')
+    owner_id = int.from_bytes(self.bot.cogs['Confessions'].crypto.decrypt(encrypted_data[-1]), 'big')
+    if owner_id != inter.author.id:
+      await inter.send(self.babel(inter, 'error_wrong_person'), ephemeral=True)
+      return
+    if len(encrypted_data) == 1: # listing
+      await inter.edit_original_message(
+        content=self.babel(inter, 'listing_withdrawn'),
+        components=None
+      )
+    elif len(encrypted_data) == 2: # offer
+      await inter.edit_original_message(
+        content=self.babel(inter, 'offer_withdrawn'),
+        components=None
+      )
+    else:
+      raise Exception("Unknown state encountered!")
 
   # Slash commands
 
@@ -137,10 +202,59 @@ class ConfessionsMarketplace(commands.Cog):
     embed.add_field('Accepted payment methods:', payment_methods, inline=True)
     embed.set_footer(text=self.babel(inter, 'shop_disclaimer'))
 
-    # We provide description as content for the spam checker, otherwise this is unused
-    await self.bot.cogs['Confessions'].confess(
-      inter, description if description else title, image=image, embed=embed
-    ) # TODO: fix image not surviving vetting
+    pendingconfession = ConfessionData(self.bot.cogs['Confessions'])
+    pendingconfession.create(inter.author, inter.channel)
+    pendingconfession.set_content(embed=embed)
+    if image:
+      await inter.response.defer(ephemeral=True)
+      await pendingconfession.add_image(attachment=image)
+    pendingconfession.channeltype_flags = 1
+
+    if vetting := await pendingconfession.check_vetting(inter):
+      await self.bot.cogs['ConfessionsModeration'].send_vetting(inter, pendingconfession, vetting)
+      return
+    if vetting is False:
+      return
+    await pendingconfession.send_confession(inter, True, webhook_override=False)
+
+  # Special ChannelType code
+  async def on_channeltype_send(
+    self, inter:disnake.Interaction, data:ConfessionData
+  ) -> dict[str] | bool:
+    if data.channeltype_flags == 1:
+      id_seller = data.parent.crypto.encrypt(data.author.id.to_bytes(8, 'big')).decode('ascii')
+      return {
+        'use_webhook': False,
+        'components': [disnake.ui.Button(
+          label=self.babel(inter.guild, 'button_offer', listing=None),
+          custom_id='confessionmarketplace_offer_'+id_seller,
+          emoji='💵',
+          style=disnake.ButtonStyle.blurple
+        ), disnake.ui.Button(
+          label=self.babel(inter.guild, 'button_withdraw', sell=True),
+          custom_id='confessionmarketplace_withdraw_'+id_seller,
+          style=disnake.ButtonStyle.grey
+        )]
+      }
+    elif data.channeltype_flags == 2:
+      listing = await inter.channel.fetch_message(data.reference_id)
+      id_seller = listing.components[0].children[0].custom_id[28:]
+      id_buyer = data.parent.crypto.encrypt(data.author.id.to_bytes(8, 'big')).decode('ascii')
+      return {
+        'use_webhook': False,
+        'components': [disnake.ui.Button(
+          label=self.babel(inter.guild, 'button_accept', listing=None),
+          custom_id='confessionmarketplace_accept_'+id_seller+'_'+id_buyer,
+          emoji='✅',
+          style=disnake.ButtonStyle.gray
+        ), disnake.ui.Button(
+          label=self.babel(inter.guild, 'button_withdraw', sell=False),
+          custom_id='confessionmarketplace_withdraw_'+id_buyer,
+          style=disnake.ButtonStyle.grey
+        )]
+      }
+    else:
+      raise Exception("Unknown state encountered!")
 
 
 def setup(bot:MerelyBot) -> None:
